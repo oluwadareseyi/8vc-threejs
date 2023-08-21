b5c630a424435ab30a6f82f8fa3baf23bc814ae6
Oluwadare Seyi <oluwadareseyii@gmail.com>
2023-08-21 15:29:20 +0100

    chore: add new models and emissive optional

--- src/main.js
+++ src/main.js
@@ -32,70 +32,158 @@ const mouse = {
 const gui = new dat.GUI();
 
 gui.add(mouse, "parallaxEnabled");
+gui.hide();
 
-init();
-animate();
 window.addEventListener("mousemove", onTouchMove, { passive: true });
 window.addEventListener("touchmove", onTouchMove, { passive: true });
 // window.addEventListener("mouseup", onTouchEnd, { passive: true });
 // window.addEventListener("touchend", onTouchEnd, { passive: true });
 
-function init() {
-  canvas = document.createElement("canvas");
-  canvas.id = "webgl";
-  document.body.appendChild(canvas);
+function loadModel(model, scene, section, i) {
+  gltfLoader.load(model, (gltf) => {
+    // gltf.scene.position.y = -0.25;
+    // console.log(gltf.scene);
+    const valuestoTweak = {
+      scale: 0.8,
+      loopAnimation: false,
+    };
 
-  // const content = document.getElementById("content");
-  sections.forEach((section, i) => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(100, 1, 0.5, 100);
-    camera.position.z = 1.5;
-    // camera.position.set(-0.3, 0, 1.5);
-    // camera.rotation.y = -0.3;
+    const { scale } = valuestoTweak;
+    const shape = gltf.scene;
+    shape.scale.set(scale, scale, scale);
+    // shape.rotation.y = 0.58;
+
+    const animation = gltf.animations[0];
+    const mixer = new THREE.AnimationMixer(shape);
+    const action = mixer.clipAction(animation);
+    action.setLoop(THREE.LoopOnce);
+    action.clampWhenFinished = true;
+    action.enable = true;
+    scene.userData.mixer = mixer;
+    scene.userData.action = action;
+
+    mixer.addEventListener("finished", () => {
+      scene.userData.animationEnded = true;
+    });
+
+    // if (i === 1) {
+    //   action.setLoop(THREE.LoopRepeat);
+    //   valuestoTweak.loopAnimation = true;
+    // }
+
+    gui
+      .add(valuestoTweak, "loopAnimation")
+      .onChange(() => {
+        action.loop = valuestoTweak.loopAnimation
+          ? THREE.LoopRepeat
+          : THREE.LoopOnce;
+
+        action.reset();
+      })
+      .name(`Loop frames ${i}`);
+
+    gui
+      .add(shape.rotation, "x")
+      .min(-10)
+      .max(2)
+      .step(0.001)
+      .name("model rotation x");
+
+    gui
+      .add(shape.rotation, "y")
+      .min(-10)
+      .max(2)
+      .step(0.001)
+      .name("model rotation y");
+
+    gui
+      .add(shape.rotation, "z")
+      .min(-10)
+      .max(2)
+      .step(0.001)
+      .name("model rotation z");
+
+    // console.log(scene);
+
+    // const camera = new THREE.PerspectiveCamera(81, 1, 0.5, 100);
+    // camera.position.z = 2;
+    let camera = gltf.cameras[0];
+    if (!camera) {
+      camera = new THREE.PerspectiveCamera(100, 1, 0.5, 100);
+      // camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.01, 200);
+      camera.position.z = 2;
+    }
     const cameraGroup = new THREE.Group();
     scene.add(cameraGroup);
 
-    if (i === 0) {
-      gui.add(camera.position, "x").min(-1).max(2).step(0.001);
-      gui.add(camera.position, "y").min(-1).max(2).step(0.001);
-      gui.add(camera.position, "z").min(-1).max(2).step(0.001);
+    // if (i === 0) {
+    //   gui.add(camera.position, "x").min(-1).max(2).step(0.001);
+    //   gui.add(camera.position, "y").min(-1).max(2).step(0.001);
+    //   gui.add(camera.position, "z").min(-1).max(2).step(0.001);
 
-      gui
-        .add(camera, "fov")
-        .min(0)
-        .max(180)
-        .step(0.001)
-        .onChange(() => {
-          camera.updateProjectionMatrix();
-        });
+    //   gui
+    //     .add(camera, "fov")
+    //     .min(0)
+    //     .max(180)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
 
-      gui
-        .add(camera, "aspect")
-        .min(0)
-        .max(3)
-        .step(0.001)
-        .onChange(() => {
-          camera.updateProjectionMatrix();
-        });
+    //   gui
+    //     .add(camera, "aspect")
+    //     .min(0)
+    //     .max(3)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
 
-      gui
-        .add(camera, "near")
-        .min(0)
-        .max(3)
-        .step(0.001)
-        .onChange(() => {
-          camera.updateProjectionMatrix();
-        });
+    //   gui
+    //     .add(camera, "near")
+    //     .min(0)
+    //     .max(3)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
 
-      gui
-        .add(camera, "far")
-        .min(0)
-        .max(100)
-        .step(0.001)
-        .onChange(() => {
-          camera.updateProjectionMatrix();
-        });
-    }
+    //   gui
+    //     .add(camera, "far")
+    //     .min(0)
+    //     .max(100)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
+
+    //   gui
+    //     .add(camera.rotation, "x")
+    //     .min(-1)
+    //     .max(2)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
+
+    //   gui
+    //     .add(camera.rotation, "y")
+    //     .min(-1)
+    //     .max(2)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
+
+    //   gui
+    //     .add(camera.rotation, "z")
+    //     .min(-1)
+    //     .max(2)
+    //     .step(0.001)
+    //     .onChange(() => {
+    //       camera.updateProjectionMatrix();
+    //     });
+    // }
 
     // the element that represents the area we want to render the scene
     scene.userData.element = section;
@@ -114,72 +202,55 @@ function init() {
     // controls.enablePan = false;
     // controls.enableZoom = false;
     // scene.userData.controls = controls;
+    const emmisiveColor = section.getAttribute("data-emissive");
 
-    const model = section.getAttribute("data-three-section");
-
-    gltfLoader.load(model, (gltf) => {
-      // gltf.scene.position.y = -0.25;
-      const valuestoTweak = {
-        scale: 0.6,
-        loopAnimation: false,
-      };
-
-      const { scale } = valuestoTweak;
-      const shape = gltf.scene;
-      shape.scale.set(scale, scale, scale);
-
-      const animation = gltf.animations[0];
-      const mixer = new THREE.AnimationMixer(shape);
-      const action = mixer.clipAction(animation);
-      action.setLoop(THREE.LoopOnce);
-      action.clampWhenFinished = true;
-      action.enable = true;
-      scene.userData.mixer = mixer;
-      scene.userData.action = action;
-
-      mixer.addEventListener("finished", () => {
-        scene.userData.animationEnded = true;
-      });
-
-      if (i === 1) {
-        action.setLoop(THREE.LoopRepeat);
-        valuestoTweak.loopAnimation = true;
-      }
-
-      gui
-        .add(valuestoTweak, "loopAnimation")
-        .onChange(() => {
-          action.loop = valuestoTweak.loopAnimation
-            ? THREE.LoopRepeat
-            : THREE.LoopOnce;
-
-          action.reset();
-        })
-        .name(`Loop frames ${i}`);
-
-      // console.log(scene);
-
-      shape.traverse((child) => {
-        if (child instanceof THREE.Mesh) {
-          child.material.emissive = new THREE.Color(0xffffff);
+    shape.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        if (emmisiveColor) {
+          child.material.emissive = new THREE.Color(emmisiveColor);
+        } else {
+          child.material.color = new THREE.Color(0xffffff);
         }
-      });
-
-      scene.add(shape);
+      }
     });
 
-    scenes.push(scene);
+    scene.add(shape);
+  });
 
-    // if (scenes.length === sections.length) {
-    //   playAnimation();
-    // }
+  scenes.push(scene);
+
+  // if (scenes.length === sections.length) {
+  //   playAnimation();
+  // }
+}
+
+function init() {
+  canvas = document.createElement("canvas");
+  canvas.id = "webgl";
+  document.body.appendChild(canvas);
+
+  // const content = document.getElementById("content");
+  sections.forEach((section, i) => {
+    const scene = new THREE.Scene();
+    // camera.position.set(-0.3, 0, 1.5);
+
+    // console.log(section);
+    // camera.rotation.y = -0.3;
+
+    const model = section.getAttribute("data-three-section").split(",");
+
+    loadModel(model, scene, section, i);
   });
 
   // for (let i = 0; i < 10; i++) {
 
   // }
 
-  renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+    alpha: true,
+  });
   // renderer.setClearColor(0xffffff, 1);
   renderer.setPixelRatio(window.devicePixelRatio);
 
@@ -284,17 +355,26 @@ function render() {
       const camera = scene.userData.camera;
       const cameraGroup = scene.userData.cameraGroup;
 
-      if (scene.userData.animationEnded) {
-        cameraGroup.rotation.y += (-cursor.x - cameraGroup.rotation.y) * 0.1;
-        cameraGroup.rotation.x += (-cursor.y - cameraGroup.rotation.x) * 0.1;
-      }
+      // if (scene.userData.animationEnded) {
+      const parallaxX = -cursor.x * 0.8;
+      const parallaxY = -cursor.y * 0.8;
+
+      cameraGroup.rotation.y += (parallaxX - cameraGroup.rotation.y) * 0.1;
+      cameraGroup.rotation.x += (parallaxY - cameraGroup.rotation.x) * 0.1;
+      // cameraGroup.rotation.z += (parallaxY - cameraGroup.rotation.z) * 0.1;
+      // }
 
       //camera.aspect = width / height; // not changing in this example
       //camera.updateProjectionMatrix();
 
-      //scene.userData.controls.update();
+      // scene.userData.controls.update();
 
       renderer.render(scene, camera);
     });
   }
 }
+
+// window.addEventListener("DOMContentLoaded", () => {
+// });
+init();
+animate();
--- tag
+++ tag
@@ -1,2 +1,8 @@
 <script type="module" src="http://localhost:3000/@vite/client"></script>
 <script type="module" src="http://localhost:3000/src/main.js"></script>
+
+<script
+  type="text/javascript"
+  async
+  src="https://uploads-ssl.webflow.com/649c36de625a16ebb7e23a94/64a2d0520232ee486a1c39a9_main.js.txt"
+></script>
